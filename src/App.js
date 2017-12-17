bd4a1674265d07f90c520bd90260af456c9d829b
Artur Komarov <artur.komarov@gmail.com>
2017-12-17 23:51:12 +0700

    Show average spread instead of range

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ReactTimeAgo from 'react-time-ago'
 import Moment from 'react-moment';
 import moment from 'moment'
 
-class SpreadRow extends React.PureComponent {
+class SpreadRow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -22,7 +22,7 @@ class SpreadRow extends React.PureComponent {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    if (JSON.stringify(nextProps) == JSON.stringify(this.props) && nextState == this.state) {
+    if (JSON.stringify(nextProps) === JSON.stringify(this.props) && nextState === this.state) {
       return false;
     } else {
       return true;
@@ -30,10 +30,10 @@ class SpreadRow extends React.PureComponent {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps == this.props) {
+    if (prevProps === this.props) {
       return;
     } else {
-      if (this.interval == undefined) {
+      if (this.interval === undefined) {
         this.setState({has_updates: true});
         this.interval = setInterval(() => this.tick(), 2000);
       }
@@ -50,17 +50,17 @@ class SpreadRow extends React.PureComponent {
 
   render() {
     return (
-      <tr class={this.state.has_updates ? 'data updated-flash' : 'data'}>
-        <td class="data-currency">{this.props.pair}</td>
-        <td class="spread-overview-spread-price mobile d-price d-price2">{this.props.spread.spreadLastPrice}%</td>
-        <td class="mobile d-price2">{this.props.spread.spreadLastPriceMinExchange[0]}</td>
-        <td class="data-exchange mobile d-price">{this.props.spread.spreadLastPriceMinExchange[1]}</td>
-        <td class="mobile d-price2">{this.props.spread.spreadLastPriceMaxExchange[0]}</td>
-        <td class="data-exchange mobile d-price">{this.props.spread.spreadLastPriceMaxExchange[1]}</td>
-        <td class="spread-overview-ba mobile d-bid">{this.props.spread.spreadBidAskMin[0]}%</td>
-        <td class="spread-overview-ba mobile d-bid">{this.props.spread.spreadBidAskMax[0]}%</td>
-        <td class="data-pair mobile-d-all"><ReactTimeAgo locale="en-GB">{new Date(this.props.spread.lastUpdated*1000)}</ReactTimeAgo></td>
-        <td class="data-pair mobile-d-all"><ReactTimeAgo locale="en-GB">{new Date(this.props.spread.timeFound*1000)}</ReactTimeAgo></td>
+      <tr className={this.state.has_updates ? 'data updated-flash' : 'data'}>
+        <td className="data-currency">{this.props.pair}</td>
+        <td className="spread-overview-spread-price mobile d-price d-price2">{this.props.spread.spreadLastPrice}%</td>
+        <td className="mobile d-price2">{this.props.spread.spreadLastPriceMinExchange[0]}</td>
+        <td className="data-exchange mobile d-price">{this.props.spread.spreadLastPriceMinExchange[1]}</td>
+        <td className="mobile d-price2">{this.props.spread.spreadLastPriceMaxExchange[0]}</td>
+        <td className="data-exchange mobile d-price">{this.props.spread.spreadLastPriceMaxExchange[1]}</td>
+        <td className="spread-overview-ba mobile d-bid">{this.props.spread.spreadBidAskMin[0]}%</td>
+        <td className="spread-overview-ba mobile d-bid">{this.props.spread.spreadBidAskMax[0]}%</td>
+        <td className="data-pair mobile-d-all"><ReactTimeAgo locale="en-GB">{new Date(this.props.spread.lastUpdated*1000)}</ReactTimeAgo></td>
+        <td className="data-pair mobile-d-all"><ReactTimeAgo locale="en-GB">{new Date(this.props.spread.timeFound*1000)}</ReactTimeAgo></td>
       </tr>
     )
   }
@@ -80,10 +80,10 @@ class StatusRow extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps == this.props) {
+    if (prevProps === this.props) {
       return;
     } else {
-      if (this.interval == undefined) {
+      if (this.interval === undefined) {
         this.setState({has_updates: true});
         this.interval = setInterval(() => this.tick(), 1000);
       }
@@ -96,7 +96,7 @@ class StatusRow extends React.Component {
 
   render() {
     return (
-      <tbody><tr><td colspan="13" class={this.state.has_updates ? 'table-updated-at mobile-d-all updated-flash' : 'table-updated-at mobile-d-all'}>{this.props.updated}</td></tr>
+      <tbody><tr><td colSpan="13" className={this.state.has_updates ? 'table-updated-at mobile-d-all updated-flash' : 'table-updated-at mobile-d-all'}>{this.props.updated}</td></tr>
       </tbody>
     )
   }
@@ -114,12 +114,12 @@ class ExchangeStatusRow extends React.Component {
   render() {
     return (
       <div>
-        <table class="d-price">
+        <table className="d-price">
           <thead>
-            <th class="mobile d-all" colspan={Object.keys(this.props.lastExchangeUpdates).length}>Exchange last update received</th>
+            <th className="mobile d-all" colSpan={Object.keys(this.props.lastExchangeUpdates).length}>Exchange last update received</th>
             <tr>
               {Object.entries(this.props.lastExchangeUpdates).map((exchange) =>
-                <td class="data-exchange mobile d-price">{exchange[0]}: <ReactTimeAgo locale="en-GB">{new Date(exchange[1]*1000)}</ReactTimeAgo></td>
+                <td className="data-exchange mobile d-price">{exchange[0]}: <ReactTimeAgo locale="en-GB">{new Date(exchange[1]*1000)}</ReactTimeAgo></td>
               )}
             </tr>
           </thead>
@@ -140,7 +140,7 @@ class PairRow extends React.Component {
   }
 
   updateSort(sortBy) {
-    if (this.state.sortBy == sortBy) {
+    if (this.state.sortBy === sortBy) {
       this.setState({sortAsc: !this.state.sortAsc})
     } else {
       this.setState({sortBy: sortBy})
@@ -150,24 +150,24 @@ class PairRow extends React.Component {
   render() {
     return (
       <div>
-        <table class="d-price">
+        <table className="d-price">
           <thead>
             <tr>
-            <th class="sortable" onClick={() => this.updateSort('pair')}><span class="glyphicon glyphicon-triangle-bottom"></span>Currency</th>
-            <th class="mobile sortable d-price d-price2" onClick={() => this.updateSort('spread')}>Price Spread</th>
-            <th class="mobile d-price2">Last Low Price</th>
-            <th class="mobile d-price">Low Exchange</th>
-            <th class="mobile d-price2">Last High Price</th>
-            <th class="mobile d-price">High Exchange</th>
-            <th class="mobile d-bid">Low Bid/Ask Spread</th>
-            <th class="mobile d-bid">High Bid/Ask Spread</th>
-            <th class="mobile-d-all">Last updated</th>
-            <th class="mobile-d-all">Time found</th>
+            <th className="sortable" onClick={() => this.updateSort('pair')}><span className="glyphicon glyphicon-triangle-bottom"></span>Currency</th>
+            <th className="mobile sortable d-price d-price2" onClick={() => this.updateSort('spread')}>Price Spread</th>
+            <th className="mobile d-price2">Last Low Price</th>
+            <th className="mobile d-price">Low Exchange</th>
+            <th className="mobile d-price2">Last High Price</th>
+            <th className="mobile d-price">High Exchange</th>
+            <th className="mobile d-bid">Low Bid/Ask Spread</th>
+            <th className="mobile d-bid">High Bid/Ask Spread</th>
+            <th className="mobile-d-all">Last updated</th>
+            <th className="mobile-d-all">Time found</th>
             </tr>
           </thead>
           {Object.entries(this.props.spreads).filter(pair => pair[1].spreadLastPrice > 0).sort(
             (pairA, pairB) => {
-              if (this.state.sortBy == 'pair') {
+              if (this.state.sortBy === 'pair') {
                 return this.state.sortAsc ? pairA[0][0] > pairB[0][0] : pairA[0][0] < pairB[0][0];
               } else {
                 return this.state.sortAsc ? pairA[1].spreadLastPrice > pairB[1].spreadLastPrice : pairA[1].spreadLastPrice < pairB[1].spreadLastPrice;
@@ -186,34 +186,34 @@ class HistoryRow extends React.Component {
   render() {
     return (
       <div>
-        <table class="d-price">
+        <table className="d-price">
           <thead>
-            <tr><td colspan="13" class="table-updated-at mobile-d-all">History</td></tr>
+            <tr><td colSpan="13" className="table-updated-at mobile-d-all">History</td></tr>
           </thead>
           <thead>
             <tr>
-            <th class="sortable" onClick={() => this.updateSort('pair')}><span class="glyphicon glyphicon-triangle-bottom"></span>Currency</th>
-            <th class="mobile sortable d-price d-price2" onClick={() => this.updateSort('spread')}>Last Price Spread</th>
-            <th class="mobile sortable d-price d-price2" onClick={() => this.updateSort('spread')}>Price Spread Min/Max</th>
-            <th class="mobile d-price2">Last Low Price</th>
-            <th class="mobile d-price">Low Exchange</th>
-            <th class="mobile d-price2">Last High Price</th>
-            <th class="mobile d-price">High Exchange</th>
-            <th class="mobile-d-all">Time found</th>
-            <th class="mobile-d-all">Was alive</th>
+            <th className="sortable" onClick={() => this.updateSort('pair')}><span className="glyphicon glyphicon-triangle-bottom"></span>Currency</th>
+            <th className="mobile sortable d-price d-price2" onClick={() => this.updateSort('spread')}>Last Price Spread</th>
+            <th className="mobile sortable d-price d-price2" onClick={() => this.updateSort('spread')}>Average Price Spread</th>
+            <th className="mobile d-price2">Last Low Price</th>
+            <th className="mobile d-price">Low Exchange</th>
+            <th className="mobile d-price2">Last High Price</th>
+            <th className="mobile d-price">High Exchange</th>
+            <th className="mobile-d-all">Time found</th>
+            <th className="mobile-d-all">Was alive</th>
             </tr>
           </thead>
           {this.props.history.map((data) =>
-            <tr class='data'>
-              <td class="data-currency">{data.pair}</td>
-              <td class="spread-overview-spread-price mobile d-price d-price2">{data.arbitrage.spread_percent.toFixed(2)}%</td>
-              <td class="spread-overview-spread-price mobile d-price d-price2">{(data.min_spread / (data.arbitrage.lowestAskPrice / 100)).toFixed(2)} - {(data.max_spread / (data.arbitrage.lowestAskPrice / 100)).toFixed(2)}%</td>
-              <td class="mobile d-price2">{data.arbitrage.lowestAskPrice.toFixed(8)}</td>
-              <td class="data-exchange mobile d-price">{data.arbitrage.lowestAskExchange}</td>
-              <td class="mobile d-price2">{data.arbitrage.highestBidPrice.toFixed(8)}</td>
-              <td class="data-exchange mobile d-price">{data.arbitrage.highestBidExchange}</td>
-              <td class="data-pair mobile-d-all"><Moment date={new Date(data.time_found*1000)} format="h:mm:ss"/></td>
-              <td class="data-pair mobile-d-all">{moment.duration(data.finished-data.time_found, "seconds").humanize()}</td>
+            <tr className='data'>
+              <td className="data-currency">{data.pair}</td>
+              <td className="spread-overview-spread-price mobile d-price d-price2">{data.arbitrage.spread_percent.toFixed(2)}%</td>
+              <td className="spread-overview-spread-price mobile d-price d-price2">{data.average_spread.toFixed(2)}%</td>
+              <td className="mobile d-price2">{data.arbitrage.lowestAskPrice.toFixed(8)}</td>
+              <td className="data-exchange mobile d-price">{data.arbitrage.lowestAskExchange}</td>
+              <td className="mobile d-price2">{data.arbitrage.highestBidPrice.toFixed(8)}</td>
+              <td className="data-exchange mobile d-price">{data.arbitrage.highestBidExchange}</td>
+              <td className="data-pair mobile-d-all"><Moment date={new Date(data.time_found*1000)} format="h:mm:ss"/></td>
+              <td className="data-pair mobile-d-all">{moment.duration(data.finished-data.time_found, "seconds").humanize()}</td>
             </tr>
           )}
         </table>
@@ -226,9 +226,13 @@ class HistoryRow extends React.Component {
 class App extends Component {
   constructor() {
     super();
+    var endpoint = "http://localhost:8081/chat";
+    if (process.env.NODE_ENV === 'production') {
+      endpoint = "http://107.170.61.147:8081/chat";
+    }
     this.state = {
       lastTimestamp: false,
-      endpoint: "http://107.170.61.147:8081/chat",
+      endpoint: endpoint,
       history: [],
       last_exchange_updates: []
     };
@@ -259,6 +263,7 @@ class App extends Component {
 
   parseHistory(data) {
     this.state.history.unshift(data)
+    this.state.history = this.state.history.slice(0, 29)
     this.setState({history: this.state.history})
   }
 
@@ -269,7 +274,7 @@ class App extends Component {
     //history.reverse();
     return (
       <div className="App">
-        <p className="App-intro">
+        <div className="App-intro">
           {this.state.last_exchange_updates ?
             <ExchangeStatusRow lastExchangeUpdates={this.state.last_exchange_updates}/> :
             <p></p>
@@ -278,12 +283,12 @@ class App extends Component {
             ? <div>
               <PairRow updated={new Date(this.response*1000).toString()} spreads={this.state.spreads}/>
             </div>
-            : <p class="loading"> Waiting for spreads data... </p> }
+            : <p className="loading"> Waiting for spreads data... </p> }
           {this.state.history.length > 0 ?
             <HistoryRow history={history}/> :
             <p></p>
           }
-        </p>
+        </div>
       </div>
     );
   }
